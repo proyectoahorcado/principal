5bfba1b6633e11dbed3410ce7020a1385c0b71af
kleer <kleer@kleer-csd-box>
2017-06-30 11:35:22 -0500

    escenario3

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require './config'
+require './lib/Escenario.rb'
 
 get '/' do	
 erb(:categorias)
 end
 
 post '/jugar'  do
-palabra="ARGENTINA"
+escenario = Escenario.new
+palabra=escenario.imprimir "ARGENTINA"
 session['palabra']=palabra
 erb(:escenario)
 end
--- lib/Escenario.rb
+++ lib/Escenario.rb
@@ -1,7 +1,7 @@
 class Escenario
  
     def imprimir palabra
-     palabra="ARGENTINA"
+     
      longitud=palabra.length
      variable= "_ "*longitud
      variable=variable.strip
